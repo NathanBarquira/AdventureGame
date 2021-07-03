36305737feccdc77a24eba9e16f7e0cc8299cbcc
NathanBarquira <75713895+NathanBarquira@users.noreply.github.com>
2021-07-02 19:52:21 -0700

    added huge changes, python stuff

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import pygame
+import pygame_functions
 
 class Game:
     def __init__(self):
@@ -9,14 +10,15 @@ class Game:
 
         # initialize player dimensions
         self.player_dim = (255, 255, 0)
-        self.player_setup = pygame.Rect(700, 300, 80, 80)
+        self.player_setup = pygame.Rect(700, 300, 80, 80)  # this handles the coordinates for the actual player
+        self.bow_setup = pygame.Rect(700, 299, 50, 50)  # this handles the coordinates for the bow
         self.player_image = pygame.image.load('alex.png')
         self.player = pygame.transform.rotate(pygame.transform.scale(self.player_image, (30, 30)), 270)
         self.game_loop()
 
     def window_setup(self):
         self.game_window.blit(self.game_background, (0, 0))
-        pygame.display.update()
+        # pygame.display.update()
 
     def handle_movement(self, pressed_key, player):
         if pressed_key[pygame.K_a] and player.x - 10 > 0:
@@ -28,9 +30,20 @@ class Game:
         if pressed_key[pygame.K_d] and player.x - 10 < 1240:
             player.x += 10
 
+    def handle_shooting(self, pressed_key, bow_location):
+        """ this method will handle the player's shooting"""
+        # TODO: add projectile later
+        if pressed_key[pygame.K_LEFT]:
+            pass
+
+
     def draw_windows(self):
         self.game_window.blit(self.player, (self.player_setup.x, self.player_setup.y))
         pygame.display.update()
+
+    def first_sprite(self):
+        """ test sprite for gif animation for shooting"""
+        test_sprite = pygame_functions.makeSprite('arrow_shoot_gif.gif', 3)
 
     def game_loop(self):
         self.clock = pygame.time.Clock()
@@ -42,9 +55,12 @@ class Game:
 
             self.pressed_key = pygame.key.get_pressed()
             self.handle_movement(pressed_key=self.pressed_key, player=self.player_setup)
+            # self.handle_shooting(pressed_key=self.pressed_key, bow_location=self.bow_)
+            # TODO: finish handling the shooting
             self.window_setup()
 
             self.draw_windows()
+
 
 
 
